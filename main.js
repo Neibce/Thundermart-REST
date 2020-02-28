685bd6e9de81839375e220d3fba4dc83aa076260
Yang Jun-Young <rex711@naver.com>
2020-02-28 14:24:17 +0900

    NODE_ENV 관련 추가

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const logger = require('morgan');
 
+console.log('NODE_ENV: ' + process.env.NODE_ENV);
+
 app.disable('x-powered-by');
 
 app.use(express.json());
@@ -11,11 +13,10 @@ const usersRouter = require('./routes/users')(app);
 app.use('/users', usersRouter);
 const itemsRouter = require('./routes/items')(app);
 app.use('/items', itemsRouter);
-
-
 app.get('*', function(req, res){
   res.status(404).json({ result: 404 });
 });
+
 
 const server = app.listen(80, function(){
 	console.log("Express server has started on port 80");
