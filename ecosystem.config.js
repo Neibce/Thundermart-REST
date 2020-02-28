19fb6db984b776be0cb09f8d91196e7b8faec789
Yang Jun-Young <rex711@naver.com>
2020-02-28 14:24:01 +0900

    production 모드 추가

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,21 @@
 module.exports = {
   apps: [{
-  name: 'api',
-  script: './main.js',
-  instances: 0,
-  exec_mode: 'cluster',
-  error_file: 'err.log',
-  out_file: 'out.log',
-  log_file: 'combined.log',
-  time: true
+		name: 'api',
+		script: './main.js',
+		watch: true,
+		
+		env: {
+			"NODE_ENV": "development" // 개발환경시 적용될 설정 지정
+		},
+		env_production: {
+			"NODE_ENV": "production" // 배포환경시 적용될 설정 지정
+		},
+
+		instances: 0,
+		exec_mode: 'cluster',
+		error_file: 'err.log',
+		out_file: 'out.log',
+		log_file: 'combined.log',
+		time: true
   }]
 }
