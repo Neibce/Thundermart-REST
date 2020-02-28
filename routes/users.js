5b5ca2fb56c918be272b15ca1899f1254b075af4
Yang Jun-Young <rex711@naver.com>
2020-02-28 14:24:48 +0900

    users 모듈화 진행

--- routes/users.js
+++ routes/users.js
@@ -1,196 +1,49 @@
 module.exports = function(app){
 	const express = require('express');
 	const router = express.Router();
-	const request = require('request');
-	const pool = require('../db_connect');
+	const users = require('../lib/users');
+	const tools = require('../lib/tools');
 
-	router.post('/login', function(req, res){
-		const client_ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+	router.post('/join', function(req, res){
+		const clientIp = tools.getClientIP(req);
 
-		if(!check_query_vaild([req.query.user_token, req.query.provider]))
-			return res.json({ res_code: 2 });
+		if(!tools.isQueryVaild([req.query.user_token, req.query.provider]))
+			return res.json({ code: 2 });
 
-		reqeust_user_login(req.query.user_token, req.query.provider, client_ip,
-			function(res_code, session_id){
-				if(res_code != 0)
-					return res.json({ res_code: res_code});
-				res.json({ res_code: res_code, session_id : session_id });
+		users.getInfoFromProvider(req.query.user_token, req.query.provider,
+			function (resCode, id, nickname){
+				if (resCode != 0)
+					return res.json({ code: resCode });
+				users.create(id, req.query.provider, nickname, clientIp,
+					function (resCode){
+						if(resCode != 0)
+							return res.json({ res_code: resCode });
+						users.createSession(id, req.query.provider, clientIp,
+							function(resCode, sessionId){
+								if(resCode != 0)
+									return res.json({ res_code: resCode });
+								res.json({ res_code : resCode, session_id : sessionId });
+							});
+						});
 			});
 	});
 
 	router.get('/already-joined', function(req, res){
-		if(!check_query_vaild([req.query.user_token, req.query.provider]))
-			return res.json({ res_code: 2 });
+		if(!tools.isQueryVaild([req.query.user_token, req.query.provider]))
+			return res.json({ code: 2 });
 
-		check_already_joined(req.query.user_token, req.query.provider,
-			function(res_code, already_joined){
-				if(res_code != 0)
-					return res.json({ res_code: res_code});
-				res.json({ res_code: res_code, already_joined : already_joined });
-			});
-	});
-
-	function check_query_vaild(query_list){
-		query_list.forEach(query => {
-			if(typeof query === 'undefined' && !query)
-				return 0;
-		});
-		return 1;
-	}
-
-	function reqeust_user_login(user_token, provider, client_ip, callback){
-		get_user_info(user_token, provider,
-			function (res_code, id, nickname){
-				if (res_code != 0)
-					return callback(res_code);
-				else
-					create_user(id, provider, nickname, client_ip,
-						function (res_code){
-							if(res_code != 0)
-								return callback(res_code);
-							else 
-								create_user_session(id, provider, client_ip,
-									function(res_code, session_id){
-										if(res_code != 0)
-											return callback(res_code);
-										else
-											return callback(res_code, session_id);
-									});
-						});
-			});
-	}
-
-	function check_already_joined(user_token, provider, callback){
-		get_user_info(user_token, provider,
-			function (res_code, id, nickname){
-				if (res_code != 0)
-					return callback(res_code);
-				else 
-					get_user_already_joined(id, provider,
-						function(res_code, already_joined) {
-							if(res_code != 0)
-								return callback(res_code);
-							else{
-								return callback(res_code, already_joined ? true : false);
-							}
-						});
-			});
-	}
-
-	function get_user_info(user_token, provider, callback){
-		if (provider == 'KAKAO'){
-			request.get('https://kapi.kakao.com/v2/user/me',{
-				headers: {
-					'Authorization' : 'Bearer ' + user_token
-				},
-				qs: { 'property_keys': '["id", "properties.nickname"]'}
-			},function (er, rs, bd) {
-				if (er){
-					console.log(er);
-					return callback(3);
-				}
-
-				var parsed_bd = JSON.parse(bd);
-
-				if(typeof parsed_bd.id === 'undefined' && !parsed_bd.id
-					|| typeof parsed_bd.properties.nickname === 'undefined' && !parsed_bd.properties.nickname){
-						if(parsed_bd.code == -10)
-							return callback(4);
-						else if (parsed_bd.code == -401)
-							return callback(101);
-						else
-							return callback(3);
-					}
-						
-
-				return callback(0, parsed_bd.id, parsed_bd.properties.nickname);
-			});
-		}else if (provider =='FACEBOOK'){
-			request.get('https://graph.facebook.com/v5.0/me?fields=id%2Cname&access_token='+ user_token,
-			function (er, rs, bd) {
-				if (er){
-					console.log(er);
-					return callback(3);
-				}
-
-				var parsed_bd = JSON.parse(bd);
-
-				if(typeof parsed_bd.id === 'undefined' && !parsed_bd.id
-					|| typeof parsed_bd.name === 'undefined' && !parsed_bd.name){
-						if(parsed_bd.error.code == 4)
-							return callback(4);
-						else if (parsed_bd.error.code == 190)
-							return callback(101);
-						else
-							return callback(3);
-					}
-
-				return callback(0, parsed_bd.id, parsed_bd.name);
-			});
-		}else
-			return callback(2);
-	}
-
-	function get_user_already_joined(provider_id, provider, callback) {
-		pool.query("SELECT COUNT(*) FROM T_USER_LIST WHERE USER_PROVIDER_ID='" + provider_id + "' AND PROVIDER='" + provider + "';",
-			function(err, data){
-				if (err){
-					console.log(err);
-					return callback(3);
-				}
-				else
-					return callback(0, data[0]['COUNT(*)']);
-			});
-	}
-
-	function create_user(provider_id, provider, nickname, client_ip, callback) {
-		get_user_already_joined(provider_id, provider,
-			function(res_code, already_joined) {
-				if(res_code != 0)
-					return callback(res_code);
-				if(!already_joined){
-					pool.query("INSERT INTO T_USER_LIST(USER_ID, USER_CREATE_IP, USER_NICKNAME, USER_PROVIDER_ID, PROVIDER) VALUES (UNHEX(REPLACE(UUID(),'-','')), '" + client_ip + "', '" + nickname + "', " + provider_id + ", '" + provider + "');",
-					function(err, data){
-						if (err){
-							console.log(err);
-							return callback(3);
-						}
-						else
-							return callback(0);
+		users.getInfoFromProvider(req.query.user_token, req.query.provider,
+			function (resCode, id, nickname){
+				if (resCode != 0)
+					return res.json({ code: resCode });
+				users.checkAlreadyJoined(id, req.query.provider,
+					function(resCode, alreadyJoined) {
+						if(resCode != 0)
+							return res.json({ res_code: resCode });
+						res.json({ res_code: resCode, already_joined : alreadyJoined });
 					});
-				} else {
-					return callback(0);
-				}
-			})
-	}
-
-	function create_user_session(provider_id, provider, client_ip, callback){
-		pool.query("DELETE FROM T_USER_SESSIONS WHERE USER_ID IN(SELECT USER_ID FROM T_USER_LIST WHERE USER_PROVIDER_ID=" + provider_id + " AND PROVIDER='" + provider + "');",
-			function(err, data){
-				if (err){
-					console.log(err);
-					return callback(3);
-				}
-				else
-					pool.query("INSERT INTO T_USER_SESSIONS (SESSION_ID, TOKEN_TYPE, USER_ID, CREATED_IP) SELECT UNHEX(REPLACE(UUID(),'-','')), 0, USER_ID, '" + client_ip + "' FROM T_USER_LIST WHERE USER_PROVIDER_ID = " + provider_id + " AND PROVIDER='" + provider + "';",
-						function(err, data){
-							if (err){
-								console.log(err);
-								return callback(3);
-							}
-							else
-								pool.query("SELECT HEX(SESSION_ID) FROM T_USER_SESSIONS WHERE USER_ID IN(SELECT USER_ID FROM T_USER_LIST WHERE USER_PROVIDER_ID=" + provider_id + " AND PROVIDER='" + provider + "');",
-									function(err, data){
-										if (err){
-											console.log(err);
-											return callback(3);
-										}
-										else
-											return callback(0, data[0]['HEX(SESSION_ID)']);
-									});
-								});
-						});
-	}
+				});
+	});
 
 	return router;
 };
